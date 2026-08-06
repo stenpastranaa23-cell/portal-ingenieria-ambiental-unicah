0ac0f2bc5fc238b0f6f906787ba1ff5fbab16944
Diego Romero <diej_romerom44@unicah.edu>
2026-08-05 20:42:58 -0600

    Ultimas correciones

--- js/contacto-mapa.js
+++ js/contacto-mapa.js
@@ -6,14 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let coordenadas = [14.1058135, -87.2047053];
 
-    let mapa = L.map("contactoMapa").setView(coordenadas, 15);
+    let mapa = L.map("contactoMapa", {
+        scrollWheelZoom: false
+    }).setView(coordenadas, 15);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
         attribution: "&copy; OpenStreetMap contributors"
     }).addTo(mapa);
 
-    L.marker(coordenadas)
+    let icono = L.divIcon({
+        className: "contacto__mapa-marcador",
+        html: "<span class=\"contacto__mapa-marcador-punto\"></span><span class=\"contacto__mapa-marcador-anillo\"></span>",
+        iconSize: [20, 20],
+        iconAnchor: [10, 10]
+    });
+
+    L.marker(coordenadas, { icon: icono })
         .addTo(mapa)
         .bindPopup("<strong>Campus Sagrado Corazón de Jesús</strong><br>Cl. Los Alcaldes, Comayagüela, Tegucigalpa")
         .openPopup();
